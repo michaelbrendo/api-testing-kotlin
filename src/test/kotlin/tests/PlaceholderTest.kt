14dec52ea003feee8ee8af1946cb31454fb0fbb2
Michael Santos <23271123+michaelbrendo@users.noreply.github.com>
2025-08-28 00:37:57 -0300

    Refactor: extract logger function to centralize usage

--- src/test/kotlin/tests/PlaceholderTest.kt
+++ src/test/kotlin/tests/PlaceholderTest.kt
@@ -1,8 +1,6 @@
 package tests
 
-import io.restassured.RestAssured
 import io.restassured.internal.RestAssuredResponseImpl
-import io.restassured.module.jsv.JsonSchemaValidator.matchesJsonSchemaInClasspath
 import org.junit.jupiter.api.Test
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
@@ -14,39 +12,40 @@ import util.enums.schemas.PlaceHolderSchemas
 @SpringBootTest
 class PlaceholderTest {
 
-    companion object{
+    companion object {
         private val logger: Logger = LoggerFactory.getLogger(PlaceholderTest::class.java)
     }
 
-    @Test
-    fun `Get posts by id`(){
-        val responseGetPostsById = PlaceholderService().getPostsById(PlaceHolderSchemas.GET_POSTS.schema) as RestAssuredResponseImpl
-
-        logger.info("Headers: " + responseGetPostsById.headers.asList())
-        logger.info("Body: " + responseGetPostsById.body.asString())
+    fun logResponse(response: RestAssuredResponseImpl) {
+        logger.info("Headers: " + response.headers.asList())
+        logger.info("Body: " + response.body.asString())
     }
 
     @Test
-    fun `Get all posts`(){
-        val responseGetAllPosts = PlaceholderService().getAllPosts(PlaceHolderSchemas.GET_ALL_POSTS.schema) as RestAssuredResponseImpl
+    fun `Get posts by id`() {
+        val response = PlaceholderService().getPostsById(PlaceHolderSchemas.GET_POSTS.schema) as RestAssuredResponseImpl
 
-        logger.info("Headers: " + responseGetAllPosts.headers.asList())
-        logger.info("Body: " + responseGetAllPosts.body.asString())
+        logResponse(response)
     }
 
     @Test
-    fun`Get post comment by id`(){
-        val responseGetPostCommentById = PlaceholderService().getPostCommentById(PlaceHolderSchemas.GET_POST_COMMENTS_BY_ID.schema) as RestAssuredResponseImpl
+    fun `Get all posts`() {
+        val response = PlaceholderService().getAllPosts(PlaceHolderSchemas.GET_ALL_POSTS.schema) as RestAssuredResponseImpl
 
-        logger.info("Headers: " + responseGetPostCommentById.headers.asList())
-        logger.info("Body: " + responseGetPostCommentById.body.asString())
+        logResponse(response)
     }
 
     @Test
-    fun`Get comment by postId`(){
-        val responseGetCommentByPostId = PlaceholderService().getCommentByPostId(PlaceHolderSchemas.GET_COMMENTS_BY_POST_ID.schema) as RestAssuredResponseImpl
+    fun `Get post comment by id`() {
+        val response = PlaceholderService().getPostCommentById(PlaceHolderSchemas.GET_POST_COMMENTS_BY_ID.schema) as RestAssuredResponseImpl
 
-        logger.info("Headers: " + responseGetCommentByPostId.headers.asList())
-        logger.info("Body: " + responseGetCommentByPostId.body.asString())
+        logResponse(response)
+    }
+
+    @Test
+    fun `Get comment by postId`() {
+        val response = PlaceholderService().getCommentByPostId(PlaceHolderSchemas.GET_COMMENTS_BY_POST_ID.schema) as RestAssuredResponseImpl
+
+        logResponse(response)
     }
 }
