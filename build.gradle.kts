6ac25922469658dd65fd1a91ec3d73e607a8846b
Michael Santos <23271123+michaelbrendo@users.noreply.github.com>
2025-09-10 12:10:40 -0300

    fix(ci): keep allure report history,

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,14 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 import org.springframework.boot.gradle.tasks.bundling.BootWar
 
+val aspectJVersion = "1.9.22"
+val allureVersion = "2.29.1"
+
+val agent: Configuration by configurations.creating {
+    isCanBeConsumed = true
+    isCanBeResolved = true
+}
+
 plugins {
 	war
 	id("org.springframework.boot") version "3.3.0"
@@ -14,6 +22,7 @@ plugins {
 group = "com.example"
 version = "0.0.1-SNAPSHOT"
 
+
 java {
 	sourceCompatibility = JavaVersion.VERSION_17
 }
@@ -23,7 +32,9 @@ repositories {
 }
 
 dependencies {
-	implementation("org.springframework.boot:spring-boot-starter-web")
+    agent("org.aspectj:aspectjweaver:$aspectJVersion")
+
+    implementation("org.springframework.boot:spring-boot-starter-web")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("org.slf4j:slf4j-api:2.0.7")
 	implementation("ch.qos.logback:logback-classic:1.4.7")
@@ -32,18 +43,14 @@ dependencies {
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 
     testImplementation("junit:junit:4.13.2")
-    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 
     testImplementation("io.rest-assured:kotlin-extensions:5.3.0")
 	testImplementation("io.rest-assured:json-schema-validator:5.3.0")
 
-    testImplementation("io.qameta.allure:allure-junit4:2.21.0")
+    testImplementation(platform("io.qameta.allure:allure-bom:$allureVersion"))
+    testImplementation("io.qameta.allure:allure-junit4")
+    testImplementation("io.qameta.allure:allure-junit4-aspect")
 }
-
-allure {
-    version.set("2.21.0")
-}
-
 
 tasks.withType<KotlinCompile> {
 	kotlinOptions {
@@ -53,10 +60,14 @@ tasks.withType<KotlinCompile> {
 }
 
 tasks.withType<Test> {
-    useJUnitPlatform() // JUnit4
-    testLogging {
-        events("passed", "skipped", "failed")
-    }
+    ignoreFailures = true
+    useJUnit() // JUnit4
+//    testLogging {
+//        events("passed", "skipped", "failed")
+//    }
+//    jvmArgs = listOf(
+//        "-javaagent:${agent.singleFile}"
+//    )
 }
 
 tasks.named<BootWar>("bootWar") {
